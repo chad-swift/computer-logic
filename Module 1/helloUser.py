acbbdfbe4bab5113f84ea069aab22f3803a800df
Chad Swift <165844586+chad-swift@users.noreply.github.com>
2024-08-30 15:49:00 -0400

    moved files to folders

---
+++ Module 1/helloUser.py
@@ -0,0 +1,25 @@
+'''
+Author: Chad Swift
+Date: 8-21-24
+Version: 1.0
+Program: helloUser.py
+'''
+
+#initialization
+STATEMENT = 'Hello, '
+
+userName = ''
+finalStatement = ''
+
+#get data
+
+userName = input('Please enter your name: ')
+
+#process data
+
+finalStatement = STATEMENT + userName
+
+#Output information
+
+print(finalStatement)
+
